385acf25ca03a5efcccde04c02cf22aa9f725497
JackTFB <jacktfbofficial@gmail.com>
2025-02-17 14:45:55 -0500

    Test 2 for portfolio

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -4,22 +4,28 @@ async function fetchPortfolioImages() {
     const path = 'Assets/Images/Portfolio';
     const url = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${path}`;
 
-    const response = await fetch(url);
-    const data = await response.json();
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
 
-    const portfolio = document.getElementsByClassName('portfolio');
-    portfolio.innerHTML = '';
+        const portfolio = document.querySelector('.portfolio');
 
-    data.forEach((file => {
-        if (file.type === 'file' && file.name.match(/\.(jpe?g|png|gif)$/)) {
-            const portfolioitem = document.createElement('div');
-            portfolioitem.className = 'portfolio-item';
-            const img = document.createElement('img');
-            img.src = file.download_url;
-            img.alt = file.name;
-            img.classList.add('portfolio-image');
-            portfolioitem.appendChild(img);
-            portfolio.appendChild(portfolioitem);
-        }
-    }))
+        data.forEach((file => {
+            if (file.type === 'file' && file.name.match(/\.(jpe?g|png|gif)$/)) {
+                const portfolioitem = document.createElement('div');
+                portfolioitem.className = 'portfolioitem';
+                const img = document.createElement('img');
+                img.src = file.download_url;
+                img.alt = file.name;
+                img.classList.add('portfolioimage');
+                portfolioitem.appendChild(img);
+                portfolio.appendChild(portfolioitem);
+            }
+        }));
+    }
+    catch (error) {
+        console.error('Error fetching images:', error);
+        const portfolio = document.querySelector('.portfolio');
+        portfolio.innerHTML = '<p>Failed to load images. Please try again later.</p>';
+    }
 }
