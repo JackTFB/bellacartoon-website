4b77f833d31f9a3a6609c19b134e87cb3005834f
JackTFB <jacktfbofficial@gmail.com>
2025-02-20 00:43:25 -0500

    Mobile Responsiveness changes

---
+++ JS/navbar.js
@@ -0,0 +1,4 @@
+document.getElementById('navbar-toggle').addEventListener('click', function() {
+    let navbarList = document.getElementById('navbarlist');
+    navbarList.classList.toggle('active');
+});
